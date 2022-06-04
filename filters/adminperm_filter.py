d353d2fc79e958664f8fe9db253b686382393131
jotty <bard143games@gmail.com>
2022-06-04 11:41:54 +0300

    Fixed non-working filter BotAdminPermissionRequired

--- filters/adminperm_filter.py
+++ filters/adminperm_filter.py
@@ -20,8 +20,8 @@ class BotAdminPermissionRequired(BaseFilter):
                        message: types.Message
                        ) -> bool:
         bot = Bot.get_current()
-        admins = [i.user for i in await bot.get_chat_administrators(message.chat.id)]
-        if await bot.get_me() not in admins:
+        is_admin = (await bot.get_chat_member(message.chat.id, (await bot.me()).id)).status == "administrator"
+        if not is_admin:
             await message.reply("Для выполнения данной команды бот должен обладать правами администратора")
             return False
 
