a342c8ffe0b2b9eb470952c3beb6a0ad6d8bdff7
jotty <bard143games@gmail.com>
2022-06-05 11:06:19 +0300

    Add roleplay commands

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from routers.help_router import help_router
 from routers.mute_router import mute_router
+from routers.ban_router import ban_router
+from routers.roleplay_router import roleplay_router
+from routers.warn_router import warn_router
 from utils import init_db
 
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +18,9 @@ dp = Dispatcher()
 
 dp.include_router(help_router)
 dp.include_router(mute_router)
+dp.include_router(ban_router)
+dp.include_router(roleplay_router)
+dp.include_router(warn_router)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
