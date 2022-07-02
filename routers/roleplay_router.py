28f1588854866bdcfc263c654e5ee00bee429426
jotty <bard143games@gmail.com>
2022-07-02 12:52:11 +0300

    added ignoring case in roleplay commands and added "kiss" command

--- routers/roleplay_router.py
+++ routers/roleplay_router.py
@@ -1,5 +1,6 @@
-from aiogram import Router, types, Bot
-from aiogram.types import MessageEntity
+from typing import Optional
+
+from aiogram import Router, types
 
 from filters.chattype_filter import ChatType
 
@@ -7,20 +8,27 @@ roleplay_router = Router()
 roleplay_router.message.bind_filter(ChatType)
 
 
-async def process_roleplay_command(message: types.Message, text: str):
+async def process_roleplay_command(message: types.Message, text: str, smile: Optional[str] = None):
     if message.reply_to_message:
-        await message.reply(f"{message.from_user.first_name} {text} {message.reply_to_message.from_user.first_name}")
+        await message.reply(f"{message.from_user.first_name} "
+                            f"{text} {message.reply_to_message.from_user.first_name} {smile}")
         return
 
     if len(splitted := message.text.split()) > 1:
-        await message.reply(f"{message.from_user.first_name} {text} {' '.join(splitted[1:])}")
+        await message.reply(f"{message.from_user.first_name} "
+                            f"{text} {' '.join(splitted[1:])} {smile}")
 
 
-@roleplay_router.message(text_startswith=["трахнуть"])
+@roleplay_router.message(text_startswith=["трахнуть"], text_ignore_case=True)
 async def trahnut_handler(message: types.Message):
-    await process_roleplay_command(message, "трахнул")
+    await process_roleplay_command(message, "трахнул(-а)", "🥰")
 
 
-@roleplay_router.message(text_startswith=["обнять"])
+@roleplay_router.message(text_startswith=["обнять"], text_ignore_case=True)
 async def hug_handler(message: types.Message):
-    await process_roleplay_command(message, "обнял")
+    await process_roleplay_command(message, "обнял(-а)", "😊")
+
+
+@roleplay_router.message(text_startswith=["поцеловать"], text_ignore_case=True)
+async def kiss_handler(message: types.Message):
+    await process_roleplay_command(message, "поцеловал(-а)", "😘")
